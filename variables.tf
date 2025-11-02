1b4f69da81e0c601d2dcf64084aa71ab9b0898e5
sayakr428 <92079116+sayakr428@users.noreply.github.com>
2025-11-02 17:03:54 +0530

    Add AWS region variable and update blueprint default

--- variables.tf
+++ variables.tf
@@ -1,9 +1,19 @@
+# variables.tf
+
+# Region for the AWS provider
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+# Project tag (used by resources/outputs)
 variable "project" {
   type    = string
   default = "cloud-butler"
 }
 
+# JSON string describing EC2/S3 to create
 variable "blueprint_json" {
   type    = string
-  default = "{}"   # empty blueprint by default
+  default = "{}"
 }
